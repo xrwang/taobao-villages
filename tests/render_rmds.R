5e3286055acfa4ff271d7cd6798c6bbedb3b8630
xrwang <xiaoweirwang@gmail.com>
2018-04-06 11:17:37 -0700

    dont render the python file for now

--- tests/render_rmds.R
+++ tests/render_rmds.R
@@ -1,4 +1,7 @@
 ## call rmarkdown on all .Rmd files
 f <- list.files(recursive = TRUE)
 Rmds <- f[grepl(".Rmd$", f)]
+remove <- ('analysis.Rmd')
+## Remove the R markdown that has the python file for now
+Rmds <- setdiff(Rmds, remove)
 lapply(Rmds, rmarkdown::render)
